bcf586360d382a1db710a4787173f90130b08fbd
grmii <antoine.thomas272@gmail.com>
2025-10-09 15:57:00 +0200

    added a login button, PAF

--- Views/user/Login.php
+++ Views/user/Login.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Page de base</title>
-    <link rel="stylesheet" href="style.css">
+    <div class="content">
+        <div class="login-rectangle">
+            <img src="assets/img/placeholder-meme.jpeg" alt="[PLACEHOLDER]Image de connexion" class="log-img">
+            <div class="rectangle-title">Connexion</div>
+            <form class="input-rectangles">
+                <label for="username"></label>
+                <input type="text" id="username" placeholder="Nom d'utilisateur" class="input-rectangle">
+
+                <label for="password"></label>
+                <input type="password" id="password" placeholder="Mot de passe" class="input-rectangle">
+
+                <button type="submit" class="input-rectangle" style="background:#1360AA;color:#fff;cursor:pointer;font-size:1.2em;">Connexion</button>
+            </form>
+            <a href="#" class="text-link">Mot de passe oublié ?</a>
+            <a href="register.html" class="text-link">Vous ne possédez pas de compte ?</a>
+        </div>
+    </div>
     <script>
         function setThemeIcon() {
             const btn = document.getElementById('theme-toggle');
@@ -29,31 +40,3 @@
         }
         window.addEventListener('DOMContentLoaded', applySavedTheme);
     </script>
-</head>
-<body>
-<header>
-    <div class="header-left">
-        <img src="assets/img/placeholder-meme.jpeg" alt="[PLACEHOLDER] Amou" class="header-logo">
-    </div>
-    <button id="theme-toggle" onclick="toggleTheme()" aria-label="Change theme"></button>
-</header>
-<div class="content">
-    <div class="login-rectangle">
-        <img src="assets/img/placeholder-meme.jpeg" alt="[PLACEHOLDER]Image de connexion" class="log-img">
-        <div class="rectangle-title">Connexion</div>
-        <div class="input-rectangles">
-            <label for="username"></label>
-            <input type="text" id="username" placeholder="Nom d'utilisateur" class="input-rectangle">
-
-            <label for="password"></label>
-            <input type="password" id="password" placeholder="Mot de passe" class="input-rectangle">
-        </div>
-        <a href="#" class="text-link">Mot de passe oublié ?</a>
-        <a href="register.html" class="text-link">Vous ne possédez pas de compte ?</a>
-    </div>
-</div>
-<footer>
-    <p style="text-align:left; margin-left:50px;">Si tu vois ça je suis un mauvais dev JE SUIS UN FOOTER</p>
-</footer>
-</body>
-</html>
