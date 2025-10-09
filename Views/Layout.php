4307c7752a3fad61ce07318638a207a37d3ee51e
Kiko <francisco.santos-jorge@etu.univ-amu.fr>
2025-10-09 13:24:43 +0200

    Add .gitignore and update Layout.php for improved structure and favicon

--- Views/Layout.php
+++ Views/Layout.php
@@ -1,3 +1,20 @@
-<?php View::show('standard/header');
-echo $A_view['body'] ;
-View::show('standard/footer'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DealTonBut</title>
+    <link rel="stylesheet" href="/public/assets/includes/styles/style.css">
+    <link rel="icon" type="image/x-icon" href="/public/assets/img/favicon.ico">
+</head>
+<body>
+    <?php View::show('standard/header'); ?>
+
+    <main>
+        echo $A_view['body']; ?>
+    </main>
+
+    <?php View::show('standard/footer'); ?>
+</body>
+</html>
+
