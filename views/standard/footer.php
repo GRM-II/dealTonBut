2d47e4314c71e7f19f7144b520fbd917909b6fa7
grmii <antoine.thomas272@gmail.com>
2025-10-15 11:47:14 +0200

    updated footer.php, I'm PLUHing my PAF

--- views/standard/footer.php
+++ views/standard/footer.php
@@ -1,7 +1,9 @@
 <?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
 <footer>
-    <div class="footer-left">
-        <img src="/public/assets/img/amU_logo.svg" alt="[PLACEHOLDER] amoU" class="footer-logo">
-        <p>Mentions légales :</p>
+    <div class="footer">
+        <img src="public/assets/img/amU.png" alt="[PLACEHOLDER] amoU" class="footer-logo">
+        <p class="footer-text">Page instagram</p>
+        <p class="footer-text">⠀|⠀</p>
+        <p class="footer-text">GRM - Kiko - Akcyl - mimojeej - Louloute - Olivier G. (Le G c'est le S)</p>
     </div>
 </footer>
