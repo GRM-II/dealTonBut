372e3d1ffccb89f986d7c58055319f5795e026ac
grmii <antoine.thomas272@gmail.com>
2025-10-15 11:53:33 +0200

    ACTUALLY changed the footer (There was PAF)

--- views/standard/footer.php
+++ views/standard/footer.php
@@ -1,7 +1,7 @@
 <?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
 <footer>
     <div class="footer">
-        <img src="public/assets/img/amU.png" alt="[PLACEHOLDER] amoU" class="footer-logo">
+        <img src="public/assets/img/amU_logo.png" alt="[PLACEHOLDER] amoU" class="footer-logo">
         <p class="footer-text">Page instagram</p>
         <p class="footer-text">⠀|⠀</p>
         <p class="footer-text">GRM - Kiko - Akcyl - mimojeej - Louloute - Olivier G. (Le G c'est le S)</p>
