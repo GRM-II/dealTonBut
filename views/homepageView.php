5d49860e05af476d5d0bbb32a0cac490b9f51c3b
grmii <antoine.thomas272@gmail.com>
2025-11-07 14:42:41 +0100

    added name of PAFject and button to desc

--- views/homepageView.php
+++ views/homepageView.php
@@ -1,5 +1,13 @@
 <main>
     <div class="content">
+        <div class="project-description-rectangle">
+            <img src="/public/assets/img/placeholder-meme.jpeg" alt="Logo du projet" class="project-logo">
+            <div class="project-text">
+                <h2 class="project-title">Nom du Projet</h2>
+                <button id="scroll-to-description" class="btn-learn-more">En savoir plus</button>
+            </div>
+        </div>
+
         <div class="login-rectangle">
             <img src="/public/assets/img/placeholder-meme.jpeg" alt="Image de connexion" class="log-img">
             <div class="rectangle-title">Connexion</div>
@@ -38,5 +46,34 @@
             <a href="#" class="text-link">Mot de passe oublié ?</a>
             <a href="index.php?controller=user&action=register" class="text-link">Vous ne possédez pas de compte ?</a>
         </div>
+
+        <div id="project-description-section" class="project-description-detailed">
+            <h2 class="project-description-title">À propos du projet</h2>
+            <p class="project-description-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <p class="project-description-text">
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+            <p class="project-description-text">
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
+            </p>
+        </div>
     </div>
 </main>
+
+<script>
+    document.getElementById('scroll-to-description').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('project-description-section').scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+</script>
+
